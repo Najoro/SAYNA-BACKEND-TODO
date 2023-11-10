16dd470c96855b49d08d36d4814c4984578becaa
najoro <najofanantenana@gmail.com>
2023-11-10 14:21:22 +0100

    style.css

--- fct/item.php
+++ fct/item.php
@@ -5,28 +5,32 @@
 
     if($itemEdit === $id){
         return ('
-        <form action="editItem.php?" method="POST">
-            <input type="hidden" name="hiddenID" value = '.$itemEdit.'>
-            <input type="text" name="resultEditItem" id="item" value ='.$item.'>
-            <span class="btm-submit">
-                <button type="submit">ok</button>
-            </span>
-        </form>
+        <div class="add-items">
+            <form action="editItem.php?" method="POST">
+                <input type="hidden" name="hiddenID" value = '.$itemEdit.'>
+                <input type="text" name="resultEditItem" id="item" value ='.$item.'>
+                <span class="btm-submit">
+                    <button type="submit">ok</button>
+                </span>
+            </form>
+        </div>
         ');
     }else{
     return ('
         <ul>
             <li>
-                <input type="checkbox" name="item" id='.$id.' value = '.$item.'> '.$item.'
-                    <div class="button">
-                        <div class="delete">
-                            <a href="deletItem.php?item='.$id.'">delete</a>
-                        </div>
-                        <div class="edit">
-                            <a href="index.php?itemEdit='.$id.'">eidt</a>
-                        </div>
-                        
+                <div class = "liste-item">
+                    <input type="checkbox" name="" id='.$id.'>
+                    <label for="'.$id.'">'.$item.'</label>
+                </div>
+                <div class="button">
+                    <div class="delete">
+                        <a href="deletItem.php?item='.$id.'">delete</a>
                     </div>
+                    <div class="edit">
+                        <a href="index.php?itemEdit="'.$id.'">edit</a>
+                    </div>
+                </div>
             </li>
         </ul>
         ');
